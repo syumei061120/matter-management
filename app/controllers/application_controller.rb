e0cc435f72ae88822b1f7acbf0b1fbaefb0d4e30
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-23 16:59:08 +0900

    deviseのパラメーター修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :administrator, :user_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :administrator_id, :user_image])
   end
 end
