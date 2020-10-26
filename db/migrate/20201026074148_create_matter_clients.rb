0b74961f42120c108b093a9613b98ebf01de84f6
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-26 16:59:52 +0900

    matter_clientモデルマイグレーション

--- db/migrate/20201026074148_create_matter_clients.rb
+++ db/migrate/20201026074148_create_matter_clients.rb
@@ -1,7 +1,8 @@
 class CreateMatterClients < ActiveRecord::Migration[6.0]
   def change
     create_table :matter_clients do |t|
-
+      t.references :matter, foreign_key: true
+      t.references :client, foreign_key: true
       t.timestamps
     end
   end
