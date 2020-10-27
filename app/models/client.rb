a0d97bb02c08db1022034f1891734d7a4f41b46e
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-27 18:47:12 +0900

    バリデーション修正

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,6 @@ class Client < ApplicationRecord
   has_many :matters, through: :matter_clients
   has_many :matter_clients
 
-
   with_options presence: true do
     validates :company
     validates :department
--- app/models/matter.rb
+++ app/models/matter.rb
@@ -7,11 +7,15 @@ class Matter < ApplicationRecord
   with_options presence: true do
     validates :matter_name
     validates :product
-    validates :priority_id
-    validates :reliability_id
-    validates :progress_id
+    validates :priority_id, numericality: { other_than: 0, message: 'を選択してください'}
+    validates :reliability_id, numericality: { other_than: 0, message: 'を選択してください'}
+    validates :progress_id, numericality: { other_than: 0, message: 'を選択してください'}
     validates :occasion
     validates :staff_in_charge
   end
+
   validates :is_edited, default:false
+  validates :sale_price, numericality: { only_integer: true }, allow_blank: true
+  validates :profit_price, numericality: { only_integer: true }, allow_blank: true
+
 end
