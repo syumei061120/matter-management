ea6767b04edfe379c1855a7090e573e3c6d030c0
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-27 17:16:55 +0900

    matter,clientモデル修正

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
 class Client < ApplicationRecord
+  has_many :matters, through: :matter_clients
   has_many :matter_clients
+
 
   with_options presence: true do
     validates :company
--- app/models/matter.rb
+++ app/models/matter.rb
@@ -1,15 +1,17 @@
 class Matter < ApplicationRecord
-  belongs_to :user
-  belongs_to :sales_staff
-  has_many   :matter_client
+  belongs_to :user, optional: true
+  belongs_to :sales_staff, optional: true
+  has_many   :clients, through: :matter_clients
+  has_many   :matter_clients
 
   with_options presence: true do
+    validates :matter_name
     validates :product
     validates :priority_id
     validates :reliability_id
     validates :progress_id
     validates :occation
     validates :staff_in_charge
-    validates :is_edited
   end
+  validates :is_edited, default:false
 end
