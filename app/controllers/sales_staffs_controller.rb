ea7f52be3491b7dabdbd6e3472bb51987b2d065c
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-29 20:40:07 +0900

    sales_staffsコントローラーにedit,updateアクション設定

--- app/controllers/sales_staffs_controller.rb
+++ app/controllers/sales_staffs_controller.rb
@@ -1,2 +1,20 @@
 class SalesStaffsController < ApplicationController
+  before_action :sales_staff_find, only: [:show, :edit, :update]
+
+  def update
+    if @sales_staff.update(sales_staff_params)
+      redirect_to matter_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def sales_staff_params
+    params.require(:sales_staff).permit(:department, :staff)
+  end
+
+  def sales_staff_find
+    @sales_staff = SalesStaff.find(params[:id])
+  end
 end
