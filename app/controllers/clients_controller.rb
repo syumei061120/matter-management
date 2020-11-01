5f48dc459a2628c3aa80fcd328e16b510f8b59f7
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-01 21:22:24 +0900

    editアクション修正

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,7 @@
 class ClientsController < ApplicationController
     before_action :matter_find, only: [:index, :create, :edit, :update]
     before_action :client_find, only: [:edit, :update, :destroy]
+    before_action :matter_client_find, only: [:update, :destroy]
 
     def index
     @matter_clients = MatterClient.where(matter_id: params[:matter_id])
@@ -22,16 +23,26 @@ class ClientsController < ApplicationController
   end
   
   def update
-    if @client.update(client_params)
-      redirect_to matter_clients_path(matter_id: params[:matter_id])
+    if MatterClient.where(client_id: params[:id]).count == 1 
+      if @client.update(client_params)
+        redirect_to matter_clients_path(matter_id: params[:matter_id])
+      else
+        render :edit
+      end
     else
-      render :edit
+      @client = Client.new(client_params)
+      if @client.valid?
+        @client.save
+        @matter_client.update(matter_id: params[:matter_id], client_id: @client.id)
+        redirect_to matter_clients_path(matter_id: params[:matter_id])
+      else
+        render :edit
+      end
     end
   end
 
   def destroy
     path = Rails.application.routes.recognize_path(request.referer)
-    @matter_client = MatterClient.find_by(matter_id: params[:matter_id], client_id: params[:id])
     if MatterClient.where(matter_id: params[:matter_id]).count == 1 
       flash[:failure] = '顧客情報が一件のため削除できませんでした'
     elsif MatterClient.where(client_id: params[:id]).count == 1 
@@ -63,5 +74,9 @@ class ClientsController < ApplicationController
 
   def client_find
     @client = Client.find(params[:id])
+  end
+
+  def matter_client_find
+    @matter_client = MatterClient.find_by(matter_id: params[:matter_id], client_id: params[:id])
   end
 end
