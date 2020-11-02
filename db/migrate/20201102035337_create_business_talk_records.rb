dcdb39f57951ea23888979f4e127d0a33be80b34
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-02 22:13:44 +0900

    business_talk_recordsマイグレート

--- db/migrate/20201102035337_create_business_talk_records.rb
+++ db/migrate/20201102035337_create_business_talk_records.rb
@@ -1,7 +1,11 @@
 class CreateBusinessTalkRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :business_talk_records do |t|
-
+      t.references :user,            foreign_key: true
+      t.references :matter,          foreign_key: true
+      t.datetime   :start,           null: false
+      t.datetime   :end,             null: false
+      t.text       :explain,         null: false
       t.timestamps
     end
   end
