12c00e53fe95a87feb489f7e5f0b329a4d979424
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-03 19:12:41 +0900

    バリデーションのエラーメッセージを修正

--- app/models/business_talk_record.rb
+++ app/models/business_talk_record.rb
@@ -3,9 +3,9 @@ class BusinessTalkRecord < ApplicationRecord
   belongs_to :user
   belongs_to :matter
 
-  with_options presence: true do
+  with_options presence: { message: 'を設定してください' }do
     validates :start_time
     validates :end_time
-    validates :explain
   end
+  validates :explain, presence: true 
 end
