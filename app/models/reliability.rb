bfcaa5e76e873e3ea38c25682048704387e2fad3
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-06 21:55:14 +0900

    Update reliability.rb

--- app/models/reliability.rb
+++ app/models/reliability.rb
@@ -1,7 +1,7 @@
 class Reliability < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '0' },
+    { id: 1, name: '0%' },
     { id: 2, name: '20%' },
     { id: 3, name: '40%' },
     { id: 4, name: '60%' },
