d7115faa99ac98471c81354371ec85374b47e439
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-06 11:41:29 +0900

    サイドバーのリストにスタイルを当てる

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,31 @@
+if (document.URL.match(/users/)){
+  document.addEventListener('DOMContentLoaded', function () {
+    const userInformationLink = document.getElementById("user-information-link")
+    userInformationLink.setAttribute("style", "color:#00ffff;")
+  });
+}
+
+if (document.URL.match(/matters/)) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const matterInformation = document.getElementById("matter-information-link");
+    matterInformation.setAttribute("style", "color:#00ffff;");
+  });
+}
+
+if (document.URL.match(/clients/)) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const matterInformation = document.getElementById("matter-information-link");
+    const clientListLink = document.getElementById("client-list-link");
+    matterInformation.setAttribute("style", "#fff;");
+    clientListLink.setAttribute("style", "color:#00ffff;");
+  });
+}
+
+if (document.URL.match(/business_talk_records$/)) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const matterInformation = document.getElementById("matter-information-link");
+    const businessTalkRecordListLink = document.getElementById("business_talk_record-list-link");
+    matterInformation.setAttribute("style", "#fff;");
+    businessTalkRecordListLink.setAttribute("style", "color:#00ffff;");
+  });
+}
