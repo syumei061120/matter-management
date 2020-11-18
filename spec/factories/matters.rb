5c0694f1669f939ef5550ad26dd52188d94e5646
syumei061120 <hideakitanaka1994@gmail.com>
2020-11-18 22:36:50 +0900

    int型の制限を超えない様に修正

--- spec/factories/matters.rb
+++ spec/factories/matters.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     matter_name { Faker::Lorem.word }
     matter_explain { Faker::Lorem.sentence }
     product { Faker::Lorem.word }
-    sale_price { Faker::Number.number(digits: 10) }
-    profit_price { Faker::Number.number(digits: 10) }
+    sale_price { Faker::Number.number(digits: 5) }
+    profit_price { Faker::Number.number(digits: 5) }
     sale_time { Faker::Date.between(from: '2021-1-1', to: '2022-12-31') }
     priority_id { Faker::Number.between(from: 1, to: 3) }
     reliability_id { Faker::Number.between(from: 1, to: 6) }
